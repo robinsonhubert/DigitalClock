8dac2615c3e90b138970c9f834f9c34daf68f404
robinsonhubert <kingthivaan01@gmail.com>
2023-03-09 21:58:14 +0530

    Digital Clock

---
+++ Assets/DigitalClock.js
@@ -0,0 +1,27 @@
+let clock = () => {
+    let date= new Date();
+    let hours =date.getHours();
+    let mins =date.getMinutes();
+    let secs= date.getSeconds();
+
+
+let period ="AM";
+if(hours==0){
+    hours=12;
+}else if(hours>=12){
+    hours=hours-12;
+    period="PM"
+}
+
+ hours = hours <10? `0${hours}`:hours;
+ mins = mins <10? `0${mins}`:mins;
+ secs = secs <10? `0${secs}`:secs;
+
+ let time= `${hours}:${mins}:${secs}`
+document.getElementById("clock").innerHTML= time;
+setTimeout(clock, 1000);
+
+};
+// call the clock function
+
+clock();
